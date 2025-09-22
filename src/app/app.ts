85f5fbc0cd24932b62e5f05cb162f15ebb9f1396
aldrink-coyo <coyo.aldrink@uab.edu.bo>
2025-09-22 10:05:20 -0400

    feat(router+a11y): routes and basic accessibility improvements

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser'; // <-- 1. Importar Title
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
-export class App {
-  protected readonly title = signal('segundo-parcial');
+export class AppComponent {
+  // 2. Inyectar el servicio Title en el constructor
+  public constructor(private titleService: Title) {
+    // 3. Establecer el título de la página
+    this.titleService.setTitle('Segundo Parcial - Home');
+  }
 }
